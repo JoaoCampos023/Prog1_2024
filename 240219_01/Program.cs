212726665e2740f7cb3836fd6f77608b418d8f48
Joao Vitor Campos <joaovcampos023@gmail.com>
2024-03-03 18:48:38 -0300

    acrescentado quebra linha

---
+++ 240219_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+// Trabalhando com variaveis C# 
+
+//Strings
+//Declarando variavel sem inicialização
+string message1 = null;
+
+//Inicializar a variavel com nulo
+string message2 = null;
+
+//Inicializar a string vazia
+string message3 = System.String.Empty; // "";
+
+// Declarar uma string com valor implicito
+var message4 = "mensagem aleatoria";
+
+Console.WriteLine( message4 );
+
+// Concatenando Strings
+string concat = (message1 == null ? "" : message1) + " " + message2 + " " + message3 + " " + message4; // if ternario, elegante
+
+Console.WriteLine( "\n" + concat );
+
+
+Console.WriteLine( 
+    "A temnperatura hoje {0:D} é {1}°C"
+    , DateTime.Now
+    , 23 
+);
+
+
+string nome = string.Empty;
+Console.WriteLine( "Qual é o seu nome?");
+nome = Console.ReadLine();
+string resultado = $"Oi, {nome}! Pare de Jaguarice!";
+Console.WriteLine(resultado);
