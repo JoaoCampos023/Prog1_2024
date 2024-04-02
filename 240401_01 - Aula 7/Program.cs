3961325cacffe606d0289e39b50481b8501ebd9e
Joao Vitor Tibes de Campos <424072@unoesc.lan>
2024-04-01 22:07:43 -0300

    aula 01/04/2024

---
+++ 240401_01 - Aula 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using _240401_01___Aula_7.Models;
+
+Console.WriteLine("Hello, World!");
+
+Custumer c1 = new Custumer();
+c1.Name = "Joao";
+c1.EmailAddress = "joaovcampos023@gmail.com";
+
+// Construtor por Instanciação
+Custumer c2 = new Custumer(2);
+c1.Name = "Joao Vitor";
+c1.EmailAddress = "joaovitorcampos23@gmail.com";
+
+// Construtor por atribuição 
+Custumer c3 = new Custumer{
+    CustumerId = 3,
+    Name = "Jaguara",
+    EmailAddress = "jaguara023@gmail.com"
+};
